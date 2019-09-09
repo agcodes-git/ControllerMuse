4ea1227ce906937a49d2055d1f16ebd4ed79cd2f
agodot <zoldy.wf@gmail.com>
2019-09-08 20:43:41 -0600

    Kind of an interesting whistling preset. I'm gonna try switching to making the right a function of the left again, based on present intervals (third/fifths etc)

--- MIDI_Xbox.py
+++ MIDI_Xbox.py
@@ -14,9 +14,11 @@ j = joysticks[0]
 pjs = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 for p in pjs: p.init()
 
-player = pygame.midi.Output(0)
+player_1 = pygame.midi.Output(0)
 #player.set_instrument(12) # bells
-player.set_instrument(15)
+#player.set_instrument(15) # bells
+#player.set_instrument(40) # violin
+player_1.set_instrument(78)
 
 axis_values = {}
 @j.event
@@ -43,8 +45,8 @@ def dpad_down_pressed(): return input_manager.pressed('ht00_down')
 def dpad_left_pressed(): return input_manager.pressed('ht00_left')
 def dpad_right_pressed(): return input_manager.pressed('ht00_right')
 
-def get_right_trigger(): return input_manager.left[0]
-def get_left_trigger(): return input_manager.right[0]
+def get_right_trigger(): return input_manager.right[0]
+def get_left_trigger(): return input_manager.left[0]
 
 def left_analog_stick():
     x_axis = 0.01 if 'ax00' not in input_manager.axes else input_manager.axes['ax00']
@@ -80,47 +82,47 @@ def to_note(letter, octave, delay=0.05):
     return 60 + 12 * octave + letter_code[letter]
 def major(base_letter, octave, delay=0.05):
     letter_code = to_note(base_letter, octave)
-    player.note_on(letter_code, 127)
+    player_1.note_on(letter_code, 127)
     time.sleep(delay)
-    player.note_on(letter_code+4, 127)
+    player_1.note_on(letter_code + 4, 127)
     time.sleep(delay)
-    player.note_on(letter_code+7, 127)
+    player_1.note_on(letter_code + 7, 127)
 def minor(base_letter, octave, delay=0.05):
     letter_code = to_note(base_letter, octave)
-    player.note_on(letter_code, 127)
+    player_1.note_on(letter_code, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 3, 127)
+    player_1.note_on(letter_code + 3, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 7, 127)
+    player_1.note_on(letter_code + 7, 127)
 def felt(base_letter, octave, delay=0.05):
     letter_code = to_note(base_letter, octave)
-    player.note_on(letter_code, 127)
+    player_1.note_on(letter_code, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 0, 127)
+    player_1.note_on(letter_code + 0, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 4, 127)
+    player_1.note_on(letter_code + 4, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 5, 127)
+    player_1.note_on(letter_code + 5, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 9, 127)
+    player_1.note_on(letter_code + 9, 127)
 
 def play(base_letter, octave, delay=0):
-    player.note_on(to_note(base_letter, octave), 127)
+    player_1.note_on(to_note(base_letter, octave), 127)
 
 def long_major(base_letter, octave, delay=0.05):
     letter_code = to_note(base_letter, octave)
-    player.note_on(letter_code, 127)
+    player_1.note_on(letter_code, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 7, 127)
+    player_1.note_on(letter_code + 7, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 16, 127)
+    player_1.note_on(letter_code + 16, 127)
 def long_minor(base_letter, octave, delay=0.05):
     letter_code = to_note(base_letter, octave)
-    player.note_on(letter_code, 127)
+    player_1.note_on(letter_code, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 7, 127)
+    player_1.note_on(letter_code + 7, 127)
     time.sleep(delay)
-    player.note_on(letter_code + 15, 127)
+    player_1.note_on(letter_code + 15, 127)
 
 def point_to_quadrant(point):
 
@@ -138,6 +140,9 @@ octave = 0
 maj_chord_function = lambda : long_major if get_left_trigger() < 0.1 else major
 min_chord_function = lambda : long_minor if get_left_trigger() < 0.1 else minor
 
+old_LQ = "CENTER"
+old_RQ = "CENTER"
+
 while True:
 
     pygame.display.flip()
@@ -151,73 +156,68 @@ while True:
     left_stick = left_analog_stick()
     right_stick = right_analog_stick()
 
-    left_quadrant = point_to_quadrant(left_stick)
+    current_LQ = point_to_quadrant(left_stick)
     bs = 30
     offset = (50, 20)
     color = (150, 0, 255)
-    pygame.draw.rect(s, color, (bs + offset[0], bs + offset[1], bs, bs), 0 if left_quadrant == "UP" else 1)
-    pygame.draw.rect(s, color, (bs + offset[0], bs * 2 + offset[1], bs, bs), 0 if left_quadrant == "CENTER" else 1)
-    pygame.draw.rect(s, color, (bs + offset[0], bs * 3 + offset[1], bs, bs), 0 if left_quadrant == "DOWN" else 1)
-    pygame.draw.rect(s, color, (0 + offset[0], bs * 2 + offset[1], bs, bs), 0 if left_quadrant == "LEFT" else 1)
-    pygame.draw.rect(s, color, (bs * 2 + offset[0], bs * 2 + offset[1], bs, bs), 0 if left_quadrant == "RIGHT" else 1)
+    pygame.draw.rect(s, color, (bs + offset[0], bs + offset[1], bs, bs), 0 if current_LQ == "UP" else 1)
+    pygame.draw.rect(s, color, (bs + offset[0], bs * 2 + offset[1], bs, bs), 0 if current_LQ == "CENTER" else 1)
+    pygame.draw.rect(s, color, (bs + offset[0], bs * 3 + offset[1], bs, bs), 0 if current_LQ == "DOWN" else 1)
+    pygame.draw.rect(s, color, (0 + offset[0], bs * 2 + offset[1], bs, bs), 0 if current_LQ == "LEFT" else 1)
+    pygame.draw.rect(s, color, (bs * 2 + offset[0], bs * 2 + offset[1], bs, bs), 0 if current_LQ == "RIGHT" else 1)
 
     left_delay = 0
-    if input_manager.pressed('left_trigger'):
-       if left_quadrant == "CENTER": play('A', octave-1, delay = left_delay)
-       if left_quadrant == "DOWN": play('F', octave-1, delay = left_delay)
-       if left_quadrant == "UP": play('C', octave, delay = left_delay)
-       if left_quadrant == "LEFT": play('G', octave-1, delay = left_delay)
-       if left_quadrant == "RIGHT": play('D', octave-1, delay = left_delay)
+    shift = 1 if get_left_trigger() else 0
 
-    right_quadrant = point_to_quadrant(right_stick)
+    if current_LQ == "DOWN":
+        if old_LQ != "DOWN": player_1.note_on(to_note('A', 0)+shift, 127)
+    else: player_1.note_off(to_note('A', 0)+shift)
+
+    if current_LQ == "LEFT":
+        if old_LQ != "LEFT": player_1.note_on(to_note('B', 0)+shift, 127)
+    else: player_1.note_off(to_note('B', 0)+shift)
+
+    if current_LQ == "UP":
+        if old_LQ != "UP": player_1.note_on(to_note('C#', 1)+shift, 127)
+    else: player_1.note_off(to_note('C#', 1)+shift)
+
+    if current_LQ == "RIGHT":
+        if old_LQ != "RIGHT": player_1.note_on(to_note('Ab', 0)+shift, 127)
+    else: player_1.note_off(to_note('Ab', 0)+shift)
+
+    old_LQ = current_LQ
+
+    current_RQ = point_to_quadrant(right_stick)
     bs = 30
     offset = (200, 20)
     color = (150, 255, 0)
-    pygame.draw.rect(s, color, (bs + offset[0], bs + offset[1], bs, bs), 0 if right_quadrant == "UP" else 1)
-    pygame.draw.rect(s, color, (bs + offset[0], bs * 2 + offset[1], bs, bs), 0 if right_quadrant == "CENTER" else 1)
-    pygame.draw.rect(s, color, (bs + offset[0], bs * 3 + offset[1], bs, bs), 0 if right_quadrant == "DOWN" else 1)
-    pygame.draw.rect(s, color, (0 + offset[0], bs * 2 + offset[1], bs, bs), 0 if right_quadrant == "LEFT" else 1)
-    pygame.draw.rect(s, color, (bs * 2 + offset[0], bs * 2 + offset[1], bs, bs), 0 if right_quadrant == "RIGHT" else 1)
+    pygame.draw.rect(s, color, (bs + offset[0], bs + offset[1], bs, bs), 0 if current_RQ == "UP" else 1)
+    pygame.draw.rect(s, color, (bs + offset[0], bs * 2 + offset[1], bs, bs), 0 if current_RQ == "CENTER" else 1)
+    pygame.draw.rect(s, color, (bs + offset[0], bs * 3 + offset[1], bs, bs), 0 if current_RQ == "DOWN" else 1)
+    pygame.draw.rect(s, color, (0 + offset[0], bs * 2 + offset[1], bs, bs), 0 if current_RQ == "LEFT" else 1)
+    pygame.draw.rect(s, color, (bs * 2 + offset[0], bs * 2 + offset[1], bs, bs), 0 if current_RQ == "RIGHT" else 1)
 
     # Depending on the quadrant I would like different responses.
-    #
     right_delay = 0
-    if input_manager.pressed('right_trigger'):
+    shift = 1 if get_right_trigger() else 0
 
-        if left_quadrant == "CENTER":
-            if right_quadrant == "CENTER": long_major('A', octave-2, delay = right_delay)
-            if right_quadrant == "DOWN": long_minor('A', octave-2, delay = right_delay)
-            if right_quadrant == "UP": felt('A', octave-1, delay = right_delay)
-            if right_quadrant == "LEFT": major('A', octave-2, delay = right_delay)
-            if right_quadrant == "RIGHT": play('A', octave-2, delay = right_delay)
+    if current_RQ == "DOWN":
+        if old_RQ != "DOWN": player_1.note_on(to_note('C#', -1)+shift, 127)
+    else: player_1.note_off(to_note('C#', -1)+shift)
 
-        if left_quadrant == "LEFT":
-            if right_quadrant == "CENTER": long_major('F', octave-2, delay = right_delay)
-            if right_quadrant == "DOWN": long_minor('F', octave-2, delay = right_delay)
-            if right_quadrant == "UP": felt('F', octave-1, delay = right_delay)
-            if right_quadrant == "LEFT": major('F', octave-2, delay = right_delay)
-            if right_quadrant == "RIGHT": play('F', octave-2, delay = right_delay)
+    if current_RQ == "RIGHT":
+        if old_RQ != "RIGHT": player_1.note_on(to_note('D', -1)+shift, 127)
+    else: player_1.note_off(to_note('D', -1)+shift)
 
-        if left_quadrant == "RIGHT":
-            if right_quadrant == "CENTER": long_major('C', octave - 2, delay=right_delay)
-            if right_quadrant == "DOWN": long_minor('C', octave - 2, delay=right_delay)
-            if right_quadrant == "UP": felt('C', octave - 1, delay=right_delay)
-            if right_quadrant == "LEFT": major('C', octave - 2, delay=right_delay)
-            if right_quadrant == "RIGHT": play('C', octave - 2, delay=right_delay)
+    if current_RQ == "UP":
+        if old_RQ != "UP": player_1.note_on(to_note('E', -1)+shift, 127)
+    else: player_1.note_off(to_note('E', -1)+shift)
 
-        if left_quadrant == "UP":
-            if right_quadrant == "CENTER": long_major('G', octave-2, delay = right_delay)
-            if right_quadrant == "DOWN": long_minor('G', octave-2, delay = right_delay)
-            if right_quadrant == "UP": felt('G', octave-1, delay = right_delay)
-            if right_quadrant == "LEFT": major('G', octave-2, delay = right_delay)
-            if right_quadrant == "RIGHT": play('G', octave-2, delay = right_delay)
+    if current_RQ == "LEFT":
+        if old_RQ != "LEFT": player_1.note_on(to_note('A', -1)+shift, 127)
+    else: player_1.note_off(to_note('A', -1)+shift)
 
-        if left_quadrant == "DOWN":
-            if right_quadrant == "CENTER": long_major('D', octave-2, delay = right_delay)
-            if right_quadrant == "DOWN": long_minor('D', octave-2, delay = right_delay)
-            if right_quadrant == "UP": felt('D', octave-1, delay = right_delay)
-            if right_quadrant == "LEFT": major('D', octave-2, delay = right_delay)
-            if right_quadrant == "RIGHT": play('D', octave-2, delay = right_delay)
+    old_RQ = current_RQ
 
 
     j.dispatch_events()
